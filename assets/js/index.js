4620aa95da6de4bb9432ceb00ca52e7b8a32202a
PoliciaFederalCPX <policiafederalcpxrj@gmail.com>
2024-11-18 15:44:30 -0300

    fix: formatacao da msg

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,20 @@
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    
     var qra = document.getElementById('qra').value;
     var id = document.getElementById('id').value;
 
     var concordaSim = document.getElementById('concordaSim').checked;
 
-    
     if (concordaSim) {
         alert("Termo de Responsabilidade enviado com sucesso! Boa reprovação no COTAR!");
 
-        
         var message = {
-            content: `**Termo de Responsabilidade Preenchido**\n\n**QRA:** ${qra}\n**ID:** ${id}\n**Concorda com o Termo:** Sim`
+            content: '``` ```\n**:COT: Termo de Responsabilidade Preenchido**\n\n**QRA:** ' + qra + '\n**ID:** ' + id + '\n**Concorda com o Termo:** Sim\n'
         };
 
-        
         var webhookUrl = 'https://discord.com/api/webhooks/1308136914326392852/DmVjlmOkKRIa7bqvadMXysunn9DUpBs8pxmlDAU6vO6j_cF28UtszdhrE6kXD0zbGP4y';
 
-        
         fetch(webhookUrl, {
             method: 'POST',
             headers: {
